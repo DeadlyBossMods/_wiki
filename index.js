c9a230fbbf2b649a2d46936ae0035ec7fe4df7c0
QartemisT <63267788+QartemisT@users.noreply.github.com>
2021-02-24 18:51:09 +0000

    Update index.js

--- index.js
+++ index.js
@@ -5,22 +5,23 @@ Insert the following code to get this to work:
 <script src="https://cdn.jsdelivr.net/npm/marked/marked.min.js"></script>
 <script src="https://raw.githubusercontent.com/DeadlyBossMods/_wiki/master/index.js"></script>
 */
-const params = new URLSearchParams(window.location.hash.replace("#","?")),
-	elem = document.getElementById("dbm-injection");
 
-const lookup = {
-	1219:	"DBM-Classic-1.13.6-Released"
-}
-
-if(params.has("p")) {
-	const page = params.get("p");
-	if(page in lookup) {
-		fetch("https://raw.githubusercontent.com/wiki/DeadlyBossMods/_wiki/" + lookup[page] + ".md").then(function(response) {
-			return response.text();
-		}).then(function(html) {
-			const div = document.createElement("div");
-			div.innerHTML = marked(html);
-			elem.appendChild(div)
-		});
+document.addEventListener("DOMContentLoaded", () => {
+	const params = new URLSearchParams(window.location.hash.replace("#","?"));
+	const lookup = {
+		1219:	"DBM-Classic-1.13.6-Released"
 	}
-}
+
+	if(params.has("p")) {
+		const page = params.get("p");
+		if(page in lookup) {
+			fetch("https://raw.githubusercontent.com/wiki/DeadlyBossMods/_wiki/" + lookup[page] + ".md")
+				.then(response => response.text())
+				.then(html => {
+					const div = document.createElement("div");
+					div.innerHTML = marked(html);
+					document.getElementById("dbm-injection").appendChild(div)
+				});
+		}
+	}
+});
