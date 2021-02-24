ee47f875241fdbf7da1d2061aef31d22ef667f86
QartemisT <63267788+QartemisT@users.noreply.github.com>
2021-02-24 18:47:34 +0000

    Using hash, as wordpress dislikes the p argument

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ Insert the following code to get this to work:
 <script src="https://cdn.jsdelivr.net/npm/marked/marked.min.js"></script>
 <script src="https://raw.githubusercontent.com/DeadlyBossMods/_wiki/master/index.js"></script>
 */
-const params = new URLSearchParams(window.location.search),
+const params = new URLSearchParams(window.location.hash.replace("#","?")),
 	elem = document.getElementById("dbm-injection");
 
 const lookup = {
 	1219:	"DBM-Classic-1.13.6-Released"
 }
 
-if(params.has("p") && params.has("f")) { // Has both a forum and a page tag
+if(params.has("p")) {
 	const page = params.get("p");
 	if(page in lookup) {
 		fetch("https://raw.githubusercontent.com/wiki/DeadlyBossMods/_wiki/" + lookup[page] + ".md").then(function(response) {
